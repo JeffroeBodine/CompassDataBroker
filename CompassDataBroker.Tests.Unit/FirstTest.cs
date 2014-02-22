42327ddf0aa9a5323ef38f90c33eafcd12611786
Jeffroe <jturner@sparkoverflow.com>
2014-02-21 23:41:06 -0500

    added test project finally

---
+++ CompassDataBroker.Tests.Unit/FirstTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace CompassDataBroker.Tests
+{
+    [TestFixture]
+    public class FirstTest
+    {
+        [Test]
+        public void MyFirstTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
